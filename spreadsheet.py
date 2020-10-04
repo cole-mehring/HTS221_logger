2083706fc739a863d808b8a5660870836ac65694
Cole <Cole>
2020-10-04 17:00:40 -0500

    Add in initial files

---
+++ spreadsheet.py
@@ -0,0 +1,58 @@
+# Libraries for google spreadsheet API and functions!
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+
+# Libraries for date and time!
+from datetime import datetime
+from datetime import date
+
+# Libraries for scraping weather off of the internet!
+import bs4
+from bs4 import BeautifulSoup
+import requests, json
+from pprint import pprint
+
+from weather_data import degToCompass
+from weather_data import get_weather
+
+# Function to find the next available row in our spreadsheet
+def next_available_row(worksheet):
+    str_list = list(filter(None, worksheet.col_values(1)))
+    return str(len(str_list)+1)
+
+# use creds to create a client to interact with the Google Drive API
+scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
+creds = ServiceAccountCredentials.from_json_keyfile_name('client_secret.json', scope)
+client = gspread.authorize(creds)
+
+# Find a workbook by name and open the first sheet
+# Make sure you use the right name here.
+sheet = client.open("temp_humidity_logger").sheet1
+
+# First, let's make a timestamp!
+now = datetime.now()
+current_time = now.strftime("%H:%M:%S") #format into text
+today = date.today()
+current_date = today.strftime("%m/%d/%Y") #format into text
+
+
+# Get the weather!
+weather = get_weather()
+
+# Let's update the next available location
+next_row = next_available_row(sheet)
+
+# Begin filling out the information!
+sheet.update_cell(next_row, 1, current_date)
+sheet.update_cell(next_row, 2, current_time)
+# sheet.update_cell(next_row, 3, internal_temperature)
+# sheet.update_cell(next_row, 4, internal_humidity)
+sheet.update_cell(next_row, 5, weather[0])
+sheet.update_cell(next_row, 6, weather[1])
+sheet.update_cell(next_row, 7, weather[2])
+sheet.update_cell(next_row, 8, weather[3])
+sheet.update_cell(next_row, 9, weather[4])
+sheet.update_cell(next_row, 10, weather[5])
+sheet.update_cell(next_row, 11, weather[6])
+sheet.update_cell(next_row, 12, weather[7])
+sheet.update_cell(next_row, 13, weather[8])
---
+++ weather_data.py
@@ -0,0 +1,60 @@
+# Python script to find current
+# weather details of any city
+# using openweathermap api
+
+def degToCompass(num):
+    val=int((num/22.5)+.5)
+    arr=["N","NNE","NE","ENE","E","ESE", "SE", "SSE","S","SSW","SW","WSW","W","WNW","NW","NNW"]
+    return arr[val]
+
+def get_weather():
+    import requests, json
+    import pytemperature
+    # Enter your API key here
+    open_weather_api_key = "2b8247bdc1ee06ed27fbc6c20d43a209"
+    
+    # base_url variable to store url
+    base_url = "http://api.openweathermap.org/data/2.5/weather?"
+    
+    # Give city name
+    city_name = 'Fargo'
+    
+    # complete_url variable to store
+    # complete url address
+    complete_url = base_url + "appid=" + open_weather_api_key + "&q=" + city_name
+    
+    # get method of requests module
+    # return response object
+    response = requests.get(complete_url)
+    
+    # json method of response object
+    # convert json format data into
+    # python format data
+    website_response = response.json()
+    
+    # Now website_response contains list of nested dictionaries
+    # Check the value of "cod" key is equal to
+    # "404", means city is found otherwise,
+    # city is not found
+    if website_response["cod"] != "404":
+        
+        # Primary Fields
+        main_formatted = website_response["main"]
+        wind_formatted = website_response["wind"]
+        weather_formatted = website_response["weather"]
+
+        # Get the informatino
+        kelvin_temperature = main_formatted["temp"]
+        current_temperature = pytemperature.k2f(kelvin_temperature)
+        current_humidity = main_formatted["humidity"]
+        current_windspeed = wind_formatted["speed"]
+        current_wind_direction = degToCompass(wind_formatted["deg"])
+        current_wind_gusts = wind_formatted["gust"]
+        current_weather = weather_formatted[0]["description"] # zeroeth index has description
+        current_pressure = main_formatted["pressure"]
+
+        return current_temperature, current_humidity, current_windspeed, current_wind_gusts, current_wind_direction, current_weather, current_humidity
+    else:
+        # Some sort of error occurred. Return errors.
+        return "Error!", "Error!", "Error!", "Error!", "Error!", "Error!", "Error!"
+
