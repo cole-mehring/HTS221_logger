7cba4b1c457f1f774dec99ed0e1a746836974cb7
Cole <Cole>
2021-01-31 12:54:16 -0600

    Update main script to save most recent measurement
    The most recent measurement will now be stored in the first
    row on sheet one, historical measurements are moved to sheet two

--- main.py
+++ main.py
@@ -25,11 +25,13 @@ def next_available_row(worksheet):
 # use creds to create a client to interact with the Google Drive API
 scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
 creds = ServiceAccountCredentials.from_json_keyfile_name('client_secret.json', scope)
-client = gspread.authorize(creds)
+gc = gspread.authorize(creds)
 
-# Find a workbook by name and open the first sheet
-# Make sure you use the right name here.
-sheet = client.open("temp_humidity_logger_test").sheet1
+# Get the spreadsheet
+logger_spreadsheet = gc.open("temp_humidity_logger")
+
+# Current measurement is on sheet 0
+sheet_current = logger_spreadsheet.get_worksheet(0)
 
 # First, let's make a timestamp!
 now = datetime.now()
@@ -38,23 +40,40 @@ today = date.today()
 current_date = today.strftime("%m/%d/%Y") #format into text
 
 # Get internal measurement
-# internal_measurement = HTS221_oneshot_measurement()
+internal_measurement = HTS221_oneshot_measurement()
 
 # Get the weather!
 weather = get_weather()
 
-# Let's update the next available location
-next_row = next_available_row(sheet)
+# Begin filling out the information for current
+sheet_current.update_cell(2, 1, current_date) # Todays Date
+sheet_current.update_cell(2, 2, current_time) # Current Time
+sheet_current.update_cell(2, 3, internal_measurement[0]) # Internal temperature
+sheet_current.update_cell(2, 4, internal_measurement[1]) # Internal humidity
+sheet_current.update_cell(2, 5, weather[0]) # External temperature
+sheet_current.update_cell(2, 6, weather[1]) # External Humidity
+sheet_current.update_cell(2, 7, weather[2]) # Wind speed
+# sheet_current.update_cell(2, 8, weather[3]) # Wind gusts
+sheet_current.update_cell(2, 8, weather[3]) # Wind Direction
+sheet_current.update_cell(2, 9, weather[4]) # Description of weather
+sheet_current.update_cell(2, 10, weather[5]) # Pressure
 
-# Begin filling out the information!
-sheet.update_cell(next_row, 1, current_date) # Todays Date
-sheet.update_cell(next_row, 2, current_time) # Current Time
-# sheet.update_cell(next_row, 3, internal_measurement[0]) # Internal temperature
-# sheet.update_cell(next_row, 4, internal_measurement[1]) # Internal humidity
-sheet.update_cell(next_row, 5, weather[0]) # External temperature
-sheet.update_cell(next_row, 6, weather[1]) # External Humidity
-sheet.update_cell(next_row, 7, weather[2]) # Wind speed
+
+# Let us fill out the historical sheet on sheet one
+sheet_history = logger_spreadsheet.get_worksheet(1)
+
+# Get the next empty row for update location
+next_row = next_available_row(sheet_history)
+
+# Begin filling out the information for history
+sheet_history.update_cell(next_row, 1, current_date) # Todays Date
+sheet_history.update_cell(next_row, 2, current_time) # Current Time
+sheet_history.update_cell(next_row, 3, internal_measurement[0]) # Internal temperature
+sheet_history.update_cell(next_row, 4, internal_measurement[1]) # Internal humidity
+sheet_history.update_cell(next_row, 5, weather[0]) # External temperature
+sheet_history.update_cell(next_row, 6, weather[1]) # External Humidity
+sheet_history.update_cell(next_row, 7, weather[2]) # Wind speed
 # sheet.update_cell(next_row, 8, weather[3]) # Wind gusts
-sheet.update_cell(next_row, 8, weather[3]) # Wind Direction
-sheet.update_cell(next_row, 9, weather[4]) # Description of weather
-sheet.update_cell(next_row, 10, weather[5]) # Pressure
+sheet_history.update_cell(next_row, 8, weather[3]) # Wind Direction
+sheet_history.update_cell(next_row, 9, weather[4]) # Description of weather
+sheet_history.update_cell(next_row, 10, weather[5]) # Pressure
